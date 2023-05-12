31970cfa259dd1f7c4592edb4d7d3d06072977aa
Diogo Braz <diogo.braz04@gmail.com>
2023-05-11 21:37:44 -0300

    add port scanner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::time::Duration;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let ip_string: &String = &args[1];
+
+    let ip: IpAddr = ip_string.parse().expect("Valid IP Address");
+    for port in 1..=65535 {
+        let conn: SocketAddr = SocketAddr::new(ip, port);
+        match TcpStream::connect_timeout(&conn, Duration::from_millis(100)) {
+            Ok(_) => println!("{:5} is open", port),
+            Err(_) => {}
+        }
+    }
 }
